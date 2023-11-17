8d10566c6fc5f61d313c4d5178d08b41505d79d3
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2023-11-17 18:58:17 +0500

    fix: Build error

--- chp06-setting-up-database/app/dashboard/layout.tsx
+++ chp06-setting-up-database/app/dashboard/layout.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className='w-fit flex flex-col font-bold text-slate-700 mb-6 bg-slate-200 p-4 rounded-lg'>
           <h1>Instructions</h1>
           <small>Using &lt;Link&gt; - Navigation is optimized, since it is without full refresh</small>
-          <small>Using &lt;a&gt; - There's a full page refresh on each page navigation!</small>
+          <small>Using &lt;a&gt; - There&apos;s a full page refresh on each page navigation!</small>
         </div>
         {children}
       </div>
